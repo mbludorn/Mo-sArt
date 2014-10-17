205331cbae7b8f35286497f1b95791ab34bf5927
SeedPaths-User <lessdismissive@email.com>
2014-10-17 13:22:54 -0600

    MosArt

---
+++ MosArt/MosArt/Controllers/WhatIsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MosArt.Controllers
+{
+    public class WhatIsController : Controller
+    {
+        //
+        // GET: /WhatIs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MosArt/MosArt/Controllers/YourArtController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MosArt.Controllers
+{
+    public class YourArtController : Controller
+    {
+        //
+        // GET: /YourArt/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
